f10aa036c07adf32f71a4dc56ca139713081a747
unnamed1164 <14520618@gm.uit.edu.vn>
2020-06-08 07:27:14 +0700

    Update ver1

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ var cors = require("cors");
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var testAPIRouter = require("./routes/testAPI");
-
+var testDBRouter = require("./routes/testDB");
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -23,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/testAPI", testAPIRouter);
+app.use("/testDB", testDBRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- dockerfile
+++ dockerfile
@@ -5,10 +5,10 @@ WORKDIR /api
 # copy package.json into the container at /api
 COPY package*.json /api/
 # install dependencies
-RUN npm install
+RUN yarn install
 # Copy the current directory contents into the container at /api
 COPY . /api/
 # Make port 80 available to the world outside this container
 EXPOSE 80
 # Run the app when the container launches
-CMD ["npm", "start"]
+CMD ["yarn", "start"]
