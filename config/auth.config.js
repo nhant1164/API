9b507a5724b912fa15953358f964e56e581f863c
Nha Nguyen <nhant@garage-standard.jp>
2020-06-09 19:35:57 +0700

    Rebuild

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "nhant-secret-key"
+  };
--- routes/testDB.js
+++ routes/testDB.js
@@ -1,20 +1,46 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const  User = require('./model')
+let  User = require("../models/user.model");
 // Variable to be sent to Frontend with Database status
 let databaseConnection = "Waiting for Database response...";
 var chris = new User({
-    name: 'Chris',
+    email: 'chris',
     username: 'sevilayha',
-    password: 'password' 
+    // password: 'password' 
   });
 router.get("/", function(req, res, next) {
-    res.send(chris.name);
+    console.log(User)
+    res.send(chris.username+ " " + chris.email);
 });
 // Connecting to MongoDB
-var mongoDB = 'mongodb://mongoDB:27017/my_database';
-
+var mongoDB = 'mongodb://localhost:27017/my_database';
+//Handle khi user signup, ở đây chỉ demo nên mình sẽ không thực hiện validate nhé.
+router.post('/signup', function(req, res){
+    var user = new User();
+ 
+    user.username = req.body.username;
+    user.email = req.body.email;
+    user.setPassword(req.body.password);
+  
+  user.save().then(function(){
+    return res.json({user: user.toAuthJSON()});
+  }).catch(next);
+});
+router.post('/login', function(req, res, next){
+    // passport.authenticate sẽ nhận vào callback cho method successs ở phần config passport
+      passport.authenticate('local', {session: false}, function(err, user, info){
+        if(err){ return next(err); }
+     
+    //Nếu thông tin user là đúng thực hiện tạo JWT và trả dữ liệu về.
+        if(user){
+          user.token = user.generateJWT();
+          return res.json({user: user.toAuthJSON()});
+        } else {
+          return res.status(422).json(info);
+        }
+      })(req, res, next);
+    });
 // mongoose.connect(mongoDB
 mongoose.connect(mongoDB, { useNewUrlParser: true });
 var db = mongoose.connection;
